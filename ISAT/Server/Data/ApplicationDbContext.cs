bcbc2cb958e6f329ef587d448cd0400da5736f65
ClereLearning <100200840+ClereLearning@users.noreply.github.com>
2023-07-03 20:44:35 -0300

    Database Creation

--- ISAT/Server/Data/ApplicationDbContext.cs
+++ ISAT/Server/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Duende.IdentityServer.EntityFramework.Options;
 using ISAT.Server.Models;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -13,5 +14,64 @@ namespace ISAT.Server.Data
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("ISAT");
+            
+            
+            builder.Entity<IdentityUser<string>>(
+                entity =>
+                {
+                    entity.ToTable(name: "User");
+                }
+                );
+
+            builder.Entity<IdentityUserLogin<string>>(
+               entity =>
+               {
+                   entity.ToTable(name: "UserLogin");
+               }
+               );
+
+            builder.Entity<IdentityUserToken<string>>(
+                entity => 
+                {
+                    entity.ToTable(name: "UserToken");
+                }
+                );
+
+            builder.Entity<IdentityRole<string>>(
+                entity =>
+                {
+                    entity.ToTable(name: "Role");
+                }
+                );
+
+            builder.Entity<IdentityUserRole<string>>(
+                entity =>
+                {
+                    entity.ToTable(name: "UserRole");
+                }
+                );
+
+            builder.Entity<IdentityUserClaim<string>>(
+                entity =>
+                {
+                    entity.ToTable(name: "UserClaim");
+                }
+                );
+
+            builder.Entity<IdentityRoleClaim<string>>(
+               entity =>
+               {
+                   entity.ToTable(name: "RoleClaim");
+               }
+               );
+            
+        }
+
     }
 }
